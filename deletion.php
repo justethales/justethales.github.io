a0b7aa1632b5d4173f477cb7e910d19e7ca2c2ac
justethales <43754506+justethales@users.noreply.github.com>
2019-04-22 16:20:21 +0100

    Create deletion.php

---
+++ deletion.php
@@ -0,0 +1,41 @@
+<?php
+header('Content-Type: application/json');
+
+$signed_request = $_POST['signed_request'];
+$data = parse_signed_request($signed_request);
+$user_id = $data['user_id'];
+
+// Start data deletion
+
+$status_url = 'https://justethales.github.io/deletion.php?id=1984'; // URL to track the deletion
+$confirmation_code = '1984'; // unique code for the deletion request
+
+$data = array(
+  'url' => $status_url,
+  'confirmation_code' => $confirmation_code
+);
+echo json_encode($data);
+
+function parse_signed_request($signed_request) {
+  list($encoded_sig, $payload) = explode('.', $signed_request, 2);
+
+  $secret = "48b8a9a49335fecfa1b2b8ec6eb135a3"; // Use your app secret here
+
+  // decode the data
+  $sig = base64_url_decode($encoded_sig);
+  $data = json_decode(base64_url_decode($payload), true);
+
+  // confirm the signature
+  $expected_sig = hash_hmac('sha256', $payload, $secret, $raw = true);
+  if ($sig !== $expected_sig) {
+    error_log('Bad Signed JSON signature!');
+    return null;
+  }
+
+  return $data;
+}
+
+function base64_url_decode($input) {
+  return base64_decode(strtr($input, '-_', '+/'));
+}
+?>
